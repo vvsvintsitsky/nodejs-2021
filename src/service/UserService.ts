df8f0e8e7bdfad9454bd6ed28944e07e05b5ecfb
Uladzislau Svintsitski <v.v.svintsitsky@gmail.com>
2021-04-10 02:47:04 +0300

    refactor: remove useless async modifier

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -4,23 +4,23 @@ import { UserStorage } from '../storage/types';
 export class UserService {
     constructor(private userStorage: UserStorage) {}
 
-    public async getById(id: string): Promise<User | undefined> {
+    public getById(id: string): Promise<User | undefined> {
         return this.userStorage.getById(id);
     }
 
-    public async getAutoSuggestUsers(loginSubstring: string, limit: number): Promise<User[]> {
+    public getAutoSuggestUsers(loginSubstring: string, limit: number): Promise<User[]> {
         return this.userStorage.getAutoSuggestUsers(loginSubstring, limit);
     }
 
-    public async create(user: User): Promise<void> {
+    public create(user: User): Promise<void> {
         return this.userStorage.create(user);
     }
 
-    public async update(id: string, userPart: User): Promise<void> {
+    public update(id: string, userPart: User): Promise<void> {
         return this.userStorage.update(id, userPart);
     }
 
-    public async markAsDeleted(id: string): Promise<void> {
+    public markAsDeleted(id: string): Promise<void> {
         return this.userStorage.markAsDeleted(id);
     }
 }
